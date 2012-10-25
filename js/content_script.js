dd3dac57d2bc50ff3debdb7e53d398ad8a2cfdba
bart <bart@bart-Vostro-420-Series.(none)>
2012-10-25 14:01:42 +0100

    Improve datetime formatting.

--- js/content_script.js
+++ js/content_script.js
@@ -1,7 +1,6 @@
 var selected_strings = [];
 var fs = document.getElementsByTagName('frame');
 if (fs.length) {
-	console.log("if");
 	for (var i = 0; i < fs.length; i++) {
 		frame_document = fs[i].contentDocument;
 		selected_strings.push(frame_document.getSelection().toString());
@@ -11,6 +10,5 @@ if (fs.length) {
 	selected_strings.push(document.getSelection().toString());
 }
 
-console.log(selected_strings);
 chrome.extension.sendMessage(selected_strings);
 
--- js/unixstamp.js
+++ js/unixstamp.js
@@ -3,12 +3,19 @@ chrome.tabs.executeScript(null, {file:"js/content_script.js"});
 chrome.extension.onMessage.addListener(function(messages) {
 	var outputs = [];
 	for (var i = 0; i < messages.length; i++) {
-		outputs.push(process(messages[i]));
+		if (messages[i].length > 0) {
+			outputs.push(process(messages[i]));
+		}
 	}
 
-	for (var i = 0; i < outputs.length; i++) {
-		document.getElementById("output").innerHTML += "<h6>" + 
-			outputs[i]['input']  + "</h6><h3>" + outputs[i]['date'] + "</h3>";
+	document.getElementById("output").innerHTML = "";
+	if (outputs.length > 0) {
+		for (var i = 0; i < outputs.length; i++) {
+			document.getElementById("output").innerHTML += "<h6>" +
+				outputs[i]['input']  + "</h6><h3>" + outputs[i]['date'] + "</h3>";
+		}
+	} else {
+		document.getElementById("output").innerHTML = "<h3>No selection detected!</h3>";
 	}
 });
 
@@ -20,9 +27,16 @@ function process(input) {
 	};
 
 	if (parseInt(input)) {
-		output_date['date'] = new Date(input * 1000).toString();
+		var d = new Date(input * 1000);
+		var date_string = _zero(d.getDate()) + "/" + _zero(d.getMonth() + 1) + "/" + d.getFullYear();
+		var time_string = _zero(d.getHours()) + ":" + _zero(d.getMinutes());
+		output_date['date'] = date_string + "&nbsp;(" + time_string + ")";
 	}
 
 	return output_date;
 }
 
+
+function _zero(number) {
+	return ("0" + number).slice(-2)
+}
